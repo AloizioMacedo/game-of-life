9300b7d8b4c01255d5fb725175964fd76ceb4645
AloizioMacedo <aloiziomacedo@gmail.com>
2022-03-31 01:57:46 -0300

    Change plotting
    Now it is a persistent plot

--- game_of_life/main.py
+++ game_of_life/main.py
@@ -5,19 +5,30 @@ import matplotlib.pyplot as plt
 from game_rules import Game
 from screen import Screen
 
+WIDTH = 31
+HEIGHT = 31
+
 
 def main():
     screen = Screen()
     game = Game(screen)
-    plt.imshow(screen.get_screen(31, 31))
+    fig, ax = plt.subplots()
+    ax.imshow(screen.get_screen(WIDTH, HEIGHT), extent=(
+        (- (WIDTH-1)/2, (WIDTH-1)/2,
+         - (HEIGHT-1)/2, (HEIGHT-1)/2)
+        ))
+    plt.ion()
     plt.show()
 
     while True:
         game.update_candidates()
-        game.kill()
         game.reproduce()
-        plt.imshow(screen.get_screen(31, 31))
-        plt.show()
+        game.kill()
+        ax.imshow(screen.get_screen(WIDTH, HEIGHT), extent=(
+            (- (WIDTH-1)/2, (WIDTH-1)/2,
+             - (HEIGHT-1)/2, (HEIGHT-1)/2)
+            ))
+        fig.canvas.flush_events()
         time.sleep(1)
 
 
